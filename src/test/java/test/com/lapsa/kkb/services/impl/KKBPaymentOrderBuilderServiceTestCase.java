c60423b6770cd9eeb480b22b4d81ea36572e13ba
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-17 18:03:47 +0600

    equalTo -> is

--- src/test/java/test/com/lapsa/kkb/services/impl/KKBPaymentOrderBuilderServiceTestCase.java
+++ src/test/java/test/com/lapsa/kkb/services/impl/KKBPaymentOrderBuilderServiceTestCase.java
@@ -20,7 +20,7 @@ public class KKBPaymentOrderBuilderServiceTestCase extends ArquillianBaseTestCas
     @Test
     public void testGenerateNewOrderId() {
 	String id = paymentOrderBuilderService.generateNewOrderId();
-	assertThat(id, allOf(not(nullValue()), not(equalTo(""))));
+	assertThat(id, allOf(not(nullValue()), not(is(""))));
 	assertThat(id.length(), lessThanOrEqualTo(15));
     }
 
@@ -30,15 +30,15 @@ public class KKBPaymentOrderBuilderServiceTestCase extends ArquillianBaseTestCas
 	KKBPaymentOrder order = paymentOrderBuilderService.buildNewPaymentOrder(amount);
 	assertThat(order, not(nullValue()));
 	assertThat(order.getOrderId(), not(nullValue()));
-	assertThat(order.getTotalAmount(), equalTo(amount));
-	assertThat(order.getCurrency(), equalTo(FinCurrency.KZT));
+	assertThat(order.getTotalAmount(), is(amount));
+	assertThat(order.getCurrency(), is(FinCurrency.KZT));
 	assertThat(order.getOperationsList(),
 		allOf(not(nullValue()), not(emptyCollectionOf(KKBPaymentOperation.class)), hasSize(1)));
 	assertTrue(order.hasOperationMerchant(TestConstants.TEST_MERCHANT_ID));
 	KKBPaymentOperation oper = order.getOperationToMerchant(TestConstants.TEST_MERCHANT_ID);
 	assertThat(oper, not(nullValue()));
-	assertThat(oper.getAmount(), equalTo(amount));
-	assertThat(oper.getMerchantId(), equalTo(TestConstants.TEST_MERCHANT_ID));
+	assertThat(oper.getAmount(), is(amount));
+	assertThat(oper.getMerchantId(), is(TestConstants.TEST_MERCHANT_ID));
     }
 
     @Test
@@ -47,16 +47,16 @@ public class KKBPaymentOrderBuilderServiceTestCase extends ArquillianBaseTestCas
 	String orderId = "21321312";
 	KKBPaymentOrder order = paymentOrderBuilderService.buildNewPaymentOrder(orderId, amount);
 	assertThat(order, not(nullValue()));
-	assertThat(order.getOrderId(), not(nullValue()));
-	assertThat(order.getTotalAmount(), equalTo(amount));
-	assertThat(order.getCurrency(), equalTo(FinCurrency.KZT));
+	assertThat(order.getOrderId(), allOf(not(nullValue()), is(orderId)));
+	assertThat(order.getTotalAmount(), is(amount));
+	assertThat(order.getCurrency(), is(FinCurrency.KZT));
 	assertThat(order.getOperationsList(),
 		allOf(not(nullValue()), not(emptyCollectionOf(KKBPaymentOperation.class)), hasSize(1)));
 	assertTrue(order.hasOperationMerchant(TestConstants.TEST_MERCHANT_ID));
 	KKBPaymentOperation oper = order.getOperationToMerchant(TestConstants.TEST_MERCHANT_ID);
 	assertThat(oper, not(nullValue()));
-	assertThat(oper.getAmount(), equalTo(amount));
-	assertThat(oper.getMerchantId(), equalTo(TestConstants.TEST_MERCHANT_ID));
+	assertThat(oper.getAmount(), is(amount));
+	assertThat(oper.getMerchantId(), is(TestConstants.TEST_MERCHANT_ID));
     }
 
     @Test
@@ -66,15 +66,15 @@ public class KKBPaymentOrderBuilderServiceTestCase extends ArquillianBaseTestCas
 	KKBPaymentOrder order = paymentOrderBuilderService.buildNewPaymentOrder(orderId, amount, FinCurrency.KZT);
 	assertThat(order, not(nullValue()));
 	assertThat(order.getOrderId(), not(nullValue()));
-	assertThat(order.getTotalAmount(), equalTo(amount));
-	assertThat(order.getCurrency(), equalTo(FinCurrency.KZT));
+	assertThat(order.getTotalAmount(), is(amount));
+	assertThat(order.getCurrency(), is(FinCurrency.KZT));
 	assertThat(order.getOperationsList(),
 		allOf(not(nullValue()), not(emptyCollectionOf(KKBPaymentOperation.class)), hasSize(1)));
 	assertTrue(order.hasOperationMerchant(TestConstants.TEST_MERCHANT_ID));
 	KKBPaymentOperation oper = order.getOperationToMerchant(TestConstants.TEST_MERCHANT_ID);
 	assertThat(oper, not(nullValue()));
-	assertThat(oper.getAmount(), equalTo(amount));
-	assertThat(oper.getMerchantId(), equalTo(TestConstants.TEST_MERCHANT_ID));
+	assertThat(oper.getAmount(), is(amount));
+	assertThat(oper.getMerchantId(), is(TestConstants.TEST_MERCHANT_ID));
     }
 
 }
