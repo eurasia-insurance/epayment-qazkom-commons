200096f30f309ed6052a4e5fadb93a35f09aa5e2
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-21 10:44:06 +0600

    praser API and tests

--- src/main/java/com/lapsa/kkb/services/impl/DefaultKKBResponseParserService.java
+++ src/main/java/com/lapsa/kkb/services/impl/DefaultKKBResponseParserService.java
@@ -5,8 +5,11 @@ import static com.lapsa.kkb.services.impl.Constants.*;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -15,9 +18,11 @@ import javax.xml.validation.Schema;
 
 import org.xml.sax.SAXException;
 
+import com.lapsa.kkb.services.KKBBankSignatureService;
 import com.lapsa.kkb.services.KKBFormatException;
 import com.lapsa.kkb.services.KKBResponseParserService;
 import com.lapsa.kkb.services.KKBServiceError;
+import com.lapsa.kkb.services.KKBWrongSignature;
 import com.lapsa.kkb.xml.KKBXmlDocument;
 import com.lapsa.kkb.xml.KKBXmlMerchant;
 
@@ -25,17 +30,22 @@ import com.lapsa.kkb.xml.KKBXmlMerchant;
 public class DefaultKKBResponseParserService extends KKBGenericService
 	implements KKBResponseParserService {
 
+    private static final String BANK_REGEX = "(\\<bank.*?\\<\\/bank\\>)";
+    private static final int BANK_REGEX_FLAGS = Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.MULTILINE
+	    | Pattern.DOTALL;
+    private static final Pattern BANK_REGEX_PATTERN = Pattern.compile(BANK_REGEX, BANK_REGEX_FLAGS);
+
     private Unmarshaller unmarshaller;
 
     // @Resource(lookup = KKB_PKI_CONFIGURATION_PROPERTIES_LOOKUP)
     // private Properties configurationProperties;
-    //
-    // @EJB
-    // private KKBMerchantSignatureService merchantSignatureService;
+
+    @EJB
+    private KKBBankSignatureService bankSignatureService;
 
     @PostConstruct
     public void init() {
-	try  {
+	try {
 	    Schema schema = loadSchemaFromResource(SCHEMA_RESPONSE);
 	    JAXBContext jaxbContext = JAXBContext.newInstance(KKBXmlMerchant.class, KKBXmlDocument.class);
 	    unmarshaller = jaxbContext.createUnmarshaller();
@@ -51,15 +61,36 @@ public class DefaultKKBResponseParserService extends KKBGenericService
     public String parseOrderId(String response) throws KKBServiceError, KKBFormatException {
 	try {
 	    KKBXmlDocument xmlDocument = (KKBXmlDocument) unmarshaller.unmarshal(new StringReader(response));
-	    if (xmlDocument == null || xmlDocument.getBank() == null || xmlDocument.getBank().getCustomer() == null
-		    || xmlDocument.getBank().getCustomer().getSourceMerchant() == null
-		    || xmlDocument.getBank().getCustomer().getSourceMerchant().getOrder() == null
-		    || xmlDocument.getBank().getCustomer().getSourceMerchant().getOrder().getOrderId() == null)
-		throw new KKBFormatException("Response format error");
 	    return xmlDocument.getBank().getCustomer().getSourceMerchant().getOrder().getOrderId();
 	} catch (JAXBException e) {
 	    throw new KKBFormatException(e);
 	}
+    }
+
+    @Override
+    public void validateSignature(String response) throws KKBFormatException, KKBServiceError, KKBWrongSignature {
+	byte[] data = parseBankElementDataFromResponse(response);
+	byte[] sign = parseBankSignDataFromResponse(response);
+	bankSignatureService.verify(data, sign);
+    }
+
+    // PRIVATE
+
+    private byte[] parseBankSignDataFromResponse(String response) throws KKBFormatException {
+	try {
+	    KKBXmlDocument xmlDocument = (KKBXmlDocument) unmarshaller.unmarshal(new StringReader(response));
+	    return xmlDocument.getBankSign().getSignature();
+	} catch (JAXBException e) {
+	    throw new KKBFormatException(e);
+	}
+    }
+
+    private byte[] parseBankElementDataFromResponse(String response) throws KKBFormatException {
+	Matcher matcher = BANK_REGEX_PATTERN.matcher(response);
+	if (matcher.find())
+	    return matcher.group().getBytes();
+	throw new KKBFormatException(
+		String.format("<bank /> element is null or empty at the response string: '%1$s'", response));
     }
 
 }
--- src/test/java/test/com/lapsa/kkb/services/impl/KKBResponseParserTestCase.java
+++ src/test/java/test/com/lapsa/kkb/services/impl/KKBResponseParserTestCase.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import com.lapsa.kkb.services.KKBFormatException;
 import com.lapsa.kkb.services.KKBResponseParserService;
 import com.lapsa.kkb.services.KKBServiceError;
+import com.lapsa.kkb.services.KKBWrongSignature;
 
 public class KKBResponseParserTestCase extends ArquillianBaseTestCase {
 
@@ -26,11 +27,24 @@ public class KKBResponseParserTestCase extends ArquillianBaseTestCase {
     }
 
     private static final String PARSE_ORDER_ID_FAILED_RESPONSE = "<document><bank name=\"Kazkommertsbank JSC\"><customer2 name=\"MR CARD\" mail=\"vadim.isaev@me.com\" phone=\"\"><merchant cert_id=\"c183d70b\" name=\"Test shop 3\"><order order_id=\"484902574738032\" amount=\"2382.05\" currency=\"398\"><department merchant_id=\"92061103\" amount=\"2382.05\"/></order></merchant><merchant_sign type=\"RSA\"/></customer2><customer2_sign type=\"RSA\"/><results2 timestamp=\"2016-06-14 15:18:02\"><payment merchant_id=\"92061103\" card=\"440564-XX-XXXX-6150\" amount=\"2382.05\" reference=\"160614151802\" approval_code=\"151802\" response_code=\"00\" Secure=\"No\" card_bin=\"\" c_hash=\"13988BBF7C6649F799F36A4808490A3E\"/></results2></bank><bank_sign cert_id=\"00c183d690\" type=\"SHA/RSA\">uHWuUQ938FNwU3ZkEip2/HGSL6niFomLvmklkg1mWOiGPCzEcQoFc5XFfTYnLwin3qtl3JsnO/yysFAjXFOBYfe5txQIWo5rnCzQ7/97n7jDHUHx58rqTLPzWwb70bYE3DuZch/cvS2gyFBbstUkzik+0YDJ/FuMwmTU4Kl/dBc=</bank_sign></document>";
-    private static final String PARSE_ORDER_ID_FAILED_ORDER_ID = "484902574738032";
 
     @Test(expected = KKBFormatException.class)
     public void testParseOrderId_Failed() throws KKBServiceError, KKBFormatException {
-	String orderId = responseParserService.parseOrderId(PARSE_ORDER_ID_FAILED_RESPONSE);
-	assertThat(orderId, allOf(not(nullValue()), is(PARSE_ORDER_ID_FAILED_ORDER_ID)));
+	responseParserService.parseOrderId(PARSE_ORDER_ID_FAILED_RESPONSE);
     }
+
+    private static final String VALIDATE_SIGNATURE_OK_RESPONSE = "<document><bank name=\"Kazkommertsbank JSC\"><customer name=\"MR CARD\" mail=\"vadim.isaev@me.com\" phone=\"\"><merchant cert_id=\"c183d70b\" name=\"Test shop 3\"><order order_id=\"484902574738032\" amount=\"2382.05\" currency=\"398\"><department merchant_id=\"92061103\" amount=\"2382.05\"/></order></merchant><merchant_sign type=\"RSA\"/></customer><customer_sign type=\"RSA\"/><results timestamp=\"2016-06-14 15:18:02\"><payment merchant_id=\"92061103\" card=\"440564-XX-XXXX-6150\" amount=\"2382.05\" reference=\"160614151802\" approval_code=\"151802\" response_code=\"00\" Secure=\"No\" card_bin=\"\" c_hash=\"13988BBF7C6649F799F36A4808490A3E\"/></results></bank><bank_sign cert_id=\"00c183d690\" type=\"SHA/RSA\">uHWuUQ938FNwU3ZkEip2/HGSL6niFomLvmklkg1mWOiGPCzEcQoFc5XFfTYnLwin3qtl3JsnO/yysFAjXFOBYfe5txQIWo5rnCzQ7/97n7jDHUHx58rqTLPzWwb70bYE3DuZch/cvS2gyFBbstUkzik+0YDJ/FuMwmTU4Kl/dBc=</bank_sign></document>";
+
+    @Test
+    public void testValidateSignature() throws KKBServiceError, KKBFormatException, KKBWrongSignature {
+	responseParserService.validateSignature(VALIDATE_SIGNATURE_OK_RESPONSE);
+    }
+
+    private static final String VALIDATE_SIGNATURE_FAILED_RESPONSE = "<document><bank name=\"Kazkommertsbank JSC\"><customer name=\"MR CARD\" mail=\"vadim.isaev@me.com\" phone=\"\"><merchant cert_id=\"c183d70b\" name=\"Test shop 3\"><order order_id=\"484902574738032\" amount=\"2382.05\" currency=\"398\"><department merchant_id=\"92061103\" amount=\"2382.05\"/></order></merchant><merchant_sign type=\"RSA\"/></customer><customer_sign type=\"RSA\"/><results timestamp=\"2016-06-14 15:18:02\"><payment merchant_id=\"92061103\" card=\"440564-XX-XXXX-6150\" amount=\"2382.05\" reference=\"160614151802\" approval_code=\"151802\" response_code=\"00\" Secure=\"No\" card_bin=\"\" c_hash=\"13988BBF7C6649F799F36A4808490A3E\"/></results></bank><bank_sign cert_id=\"00c183d690\" type=\"SHA/RSA\">uHWuUQ938FNwU3ZkEip2/HGSL6niFomLvmklkg1mWOiGPCzEcQoFc5XFfTYnLwin3qtl3JsnO/yysFAjXFOBYfe5txQIWo5rnCzQ7/97n7jDHUHx58rqQLPzWwb70bYE3DuZch/cvS2gyFBbstUkzik+0YDJ/FuMwmTU4Kl/dBc=</bank_sign></document>";
+
+    @Test(expected = KKBWrongSignature.class)
+    public void testValidateSignatureFailed() throws KKBServiceError, KKBFormatException, KKBWrongSignature {
+	responseParserService.validateSignature(VALIDATE_SIGNATURE_FAILED_RESPONSE);
+    }
+
 }
