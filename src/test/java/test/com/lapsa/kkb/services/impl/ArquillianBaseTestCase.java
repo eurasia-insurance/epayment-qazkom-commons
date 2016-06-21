9258eaa30742b2d30352b73f267dafd7bd9135f5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-21 19:49:18 +0600

    logging

--- src/test/java/test/com/lapsa/kkb/services/impl/ArquillianBaseTestCase.java
+++ src/test/java/test/com/lapsa/kkb/services/impl/ArquillianBaseTestCase.java
@@ -1,5 +1,7 @@
 package test.com.lapsa.kkb.services.impl;
 
+import java.util.logging.Logger;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -10,12 +12,11 @@ import com.lapsa.kkb.services.impl.DefaultKKBMerchantSignatureService;
 
 @RunWith(Arquillian.class)
 public abstract class ArquillianBaseTestCase {
-
     @Deployment
     public static JavaArchive createDeployment() {
 	JavaArchive jar = ShrinkWrap.create(JavaArchive.class);
 	jar.addPackages(true, DefaultKKBMerchantSignatureService.class.getPackage());
-	System.out.println(jar.toString(true));
+	Logger.getAnonymousLogger().fine(jar.toString(true));
 	return jar;
     }
 }
