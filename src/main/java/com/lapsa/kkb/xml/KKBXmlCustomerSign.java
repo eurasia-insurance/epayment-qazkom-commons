2ab4fa5b5e8b4ffba3721b427b000a3c3f4cd7d3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-23 09:24:36 +0600

    reflection used for hashCode and equals

--- src/main/java/com/lapsa/kkb/xml/KKBXmlCustomerSign.java
+++ src/main/java/com/lapsa/kkb/xml/KKBXmlCustomerSign.java
@@ -6,9 +6,6 @@ import javax.xml.bind.annotation.XmlAccessorOrder;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
 @XmlRootElement(name = "customer_sign")
@@ -25,24 +22,5 @@ public class KKBXmlCustomerSign extends KKBXmlGenericSign {
     @Override
     protected int getMultiplier() {
 	return MULTIPLIER;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-	if (other == null || other.getClass() != getClass())
-	    return false;
-	if (other == this)
-	    return true;
-	KKBXmlCustomerSign that = (KKBXmlCustomerSign) other;
-	return new EqualsBuilder()
-		.appendSuper(super.equals(that))
-		.isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-	return new HashCodeBuilder(getPrime(), getMultiplier())
-		.appendSuper(super.hashCode())
-		.toHashCode();
     }
 }
