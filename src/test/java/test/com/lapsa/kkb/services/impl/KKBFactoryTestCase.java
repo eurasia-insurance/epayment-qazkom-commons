c19d56a662ef036dfa57215be5c77054c0971c42
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-11 12:42:18 +0600

    test case for payment link generation

--- src/test/java/test/com/lapsa/kkb/services/impl/KKBFactoryTestCase.java
+++ src/test/java/test/com/lapsa/kkb/services/impl/KKBFactoryTestCase.java
@@ -3,6 +3,9 @@ package test.com.lapsa.kkb.services.impl;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.ejb.EJB;
 
 import org.junit.Test;
@@ -21,4 +24,11 @@ public class KKBFactoryTestCase extends ArquillianBaseTestCase {
 	assertThat(id.length(), lessThanOrEqualTo(15));
     }
 
+    
+    @Test
+    public void testGeneratePaymentURL() throws MalformedURLException {
+	String id = "66778899";
+	URL url = factory.generatePaymentPageUrl(id);
+	assertThat(url, allOf(notNullValue(), is(new URL("http://localhost:8080/order/payment/?order=66778899"))));
+    }
 }
