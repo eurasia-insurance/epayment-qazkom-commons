0141b3e3e6ad5c436e5f8cf0809819f08c742b13
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-08 12:27:41 +0600

    payment builder & test

--- src/main/java/tech/lapsa/epayment/qazkom/xml/bind/XmlDocumentPayment.java
+++ src/main/java/tech/lapsa/epayment/qazkom/xml/bind/XmlDocumentPayment.java
@@ -1,5 +1,7 @@
 package tech.lapsa.epayment.qazkom.xml.bind;
 
+import java.security.cert.X509Certificate;
+
 import javax.xml.bind.annotation.XmlAccessOrder;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorOrder;
@@ -8,6 +10,10 @@ import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import tech.lapsa.epayment.qazkom.xml.schema.XmlSchemas;
+import tech.lapsa.java.commons.function.MyArrays;
+import tech.lapsa.java.commons.function.MyObjects;
+import tech.lapsa.java.commons.function.MyStrings;
+import tech.lapsa.java.commons.security.MySignatures.VerifyingSignature;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
@@ -17,7 +23,8 @@ public class XmlDocumentPayment extends AXmlBase {
     private static final long serialVersionUID = 1L;
     private static final int PRIME = 37;
 
-    private static final SerializationTool<XmlDocumentPayment> TOOL = SerializationTool.forClass(XmlDocumentPayment.class,
+    private static final SerializationTool<XmlDocumentPayment> TOOL = SerializationTool.forClass(
+	    XmlDocumentPayment.class,
 	    XmlSchemas.PAYMENT_SCHEMA);
 
     public static final SerializationTool<XmlDocumentPayment> getTool() {
@@ -25,7 +32,62 @@ public class XmlDocumentPayment extends AXmlBase {
     }
 
     public static XmlDocumentPayment of(final String rawXml) {
+	MyStrings.requireNonEmpty(rawXml, "rawXml");
 	return TOOL.deserializeFrom(rawXml);
+    }
+
+    public static XmlDocumentPaymentBuilder builder() {
+	return new XmlDocumentPaymentBuilder();
+    }
+
+    public static final class XmlDocumentPaymentBuilder {
+	private String rawXml;
+	private VerifyingSignature signature;
+	private X509Certificate certificate;
+
+	private XmlDocumentPaymentBuilder() {
+	}
+
+	public XmlDocumentPaymentBuilder ofRawXml(String rawXml) {
+	    this.rawXml = MyStrings.requireNonEmpty(rawXml, "rawXml");
+	    return this;
+	}
+
+	public XmlDocumentPaymentBuilder checkingSignatureWith(VerifyingSignature signature) {
+	    this.signature = MyObjects.requireNonNull(signature, "signature");
+	    return this;
+	}
+
+	public XmlDocumentPaymentBuilder checkingCertificateWith(X509Certificate certificate) {
+	    this.certificate = MyObjects.requireNonNull(certificate, "certificate");
+	    return this;
+	}
+
+	public XmlDocumentPayment build() {
+	    MyStrings.requireNonEmpty(rawXml, "rawXml");
+	    XmlDocumentPayment document = TOOL.deserializeFrom(rawXml);
+
+	    if (MyObjects.nonNull(signature)) {
+		String[] banks = XmlBank.bankXmlElementsFrom(rawXml);
+		if (banks.length != 1)
+		    throw new IllegalArgumentException(
+			    "Failed to parse for single element <bank> from source XML document");
+		byte[] data = banks[0].getBytes();
+		byte[] digest = document.getBankSign().getSignature();
+		MyArrays.reverse(digest);
+		boolean signatureValid = signature.verify(data, digest);
+		if (!signatureValid)
+		    throw new IllegalStateException("Signature is invalid");
+	    }
+	    if (MyObjects.nonNull(certificate)) {
+		boolean certNumberValid = certificate.getSerialNumber()
+			.equals(document.getBankSign().getCertificateSerialNumber());
+		if (!certNumberValid)
+		    throw new IllegalStateException("Certificate serial number is not valid");
+	    }
+	    return document;
+	}
+
     }
 
     @Override
---
+++ src/test/java/test/builder/XmlDocumentPaymentBulderTest.java
@@ -0,0 +1,88 @@
+package test.builder;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import tech.lapsa.epayment.qazkom.xml.bind.XmlDocumentPayment;
+import tech.lapsa.java.commons.resources.Resources;
+import tech.lapsa.java.commons.security.MyCertificates;
+import tech.lapsa.java.commons.security.MyKeyStores;
+import tech.lapsa.java.commons.security.MyKeyStores.StoreType;
+import tech.lapsa.java.commons.security.MySignatures;
+import tech.lapsa.java.commons.security.MySignatures.Algorithm;
+import tech.lapsa.java.commons.security.MySignatures.VerifyingSignature;
+
+public class XmlDocumentPaymentBulderTest {
+
+    private static final StoreType STORETYPE = StoreType.JKS;
+    private static final String KEYSTORE = "/kkb.jks";
+    private static final String STOREPASS = "1q2w3e4r";
+    private static final String ALIAS = "kkbca-test";
+    private static final Algorithm ALGORITHM = Algorithm.SHA1withRSA;
+
+    private static X509Certificate certificate;
+    private static VerifyingSignature signature;
+
+    @BeforeClass
+    public static void loadKeys() throws Exception {
+
+	InputStream storeStream = Resources.optionalAsStream(XmlDocumentPaymentBulderTest.class, KEYSTORE) //
+		.orElseThrow(() -> new RuntimeException("Keystore not found"));
+
+	KeyStore keystore = MyKeyStores.from(storeStream, STORETYPE, STOREPASS) //
+		.orElseThrow(() -> new RuntimeException("Can not load keystore"));
+
+	certificate = MyCertificates.from(keystore, ALIAS) //
+		.orElseThrow(() -> new RuntimeException("Can find key entry"));
+
+	signature = MySignatures.forVerification(certificate, ALGORITHM) //
+		.orElseThrow(() -> new RuntimeException("Can't process with signing signature"));
+
+    }
+
+    @Test
+    public void simpleBuildTest() {
+	final String BANK_XML = ""
+		+ "<bank name=\"Kazkommertsbank JSC\">"
+		+ "<customer name=\"MR CARD\" mail=\"vadim.isaev@me.com\" phone=\"\">"
+		+ "<merchant cert_id=\"c183d70b\" name=\"Test shop 3\">"
+		+ "<order order_id=\"484902574738032\" amount=\"2382.05\" currency=\"398\">"
+		+ "<department merchant_id=\"92061103\" amount=\"2382.05\"/>"
+		+ "</order>"
+		+ "</merchant>"
+		+ "<merchant_sign type=\"RSA\"/>"
+		+ "</customer>"
+		+ "<customer_sign type=\"RSA\"/>"
+		+ "<results timestamp=\"2016-06-14 15:18:02\">"
+		+ "<payment merchant_id=\"92061103\" card=\"440564-XX-XXXX-6150\" amount=\"2382.05\" "
+		+ "reference=\"160614151802\" approval_code=\"151802\" response_code=\"00\" Secure=\"No\" "
+		+ "card_bin=\"\" c_hash=\"13988BBF7C6649F799F36A4808490A3E\"/>"
+		+ "</results>"
+		+ "</bank>";
+
+	final String PAYMENT_XML = ""
+		+ "<document>"
+		+ BANK_XML
+		+ "<bank_sign cert_id=\"c183d690\" type=\"SHA/RSA\">"
+		+ "uHWuUQ938FNwU3ZkEip2/HGSL6niFomLvmk"
+		+ "lkg1mWOiGPCzEcQoFc5XFfTYnLwin3qtl3JsnO/yysFAjXF"
+		+ "OBYfe5txQIWo5rnCzQ7/97n7jDHUHx58rqTLPzWwb70bYE3DuZch/cvS2gyFBbstUkzik+0YDJ/FuMwmTU4Kl/dBc="
+		+ "</bank_sign>"
+		+ "</document>";
+
+	XmlDocumentPayment o = XmlDocumentPayment.builder() //
+		.checkingCertificateWith(certificate) //
+		.checkingSignatureWith(signature) //
+		.ofRawXml(PAYMENT_XML)
+		.build();
+	assertThat(o, not(nullValue()));
+    }
+
+}
