b7d2ba871a7ed98b850d1801073b69241052fab5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-08 12:15:45 +0600

    #frmt

--- src/test/java/test/mapping/XmlDocumentPaymentTest.java
+++ src/test/java/test/mapping/XmlDocumentPaymentTest.java
@@ -34,7 +34,24 @@ public class XmlDocumentPaymentTest {
 
     private static final XmlDocumentPayment TEST_DOCUMENT_AS_OBJECT;
 
-    private static final String TEST_DOCUMENT_AS_PLAINTEXT = "<document><bank name=\"Kazkommertsbank JSC\"><customer mail=\"klient@mymail.com\" name=\"John Cardholder\" phone=\"223322\"><merchant cert_id=\"7269c18d00010000005e\" name=\"Shop Name\"><order order_id=\"000282\" currency=\"398\" amount=\"3100\"><department RL=\"ASDFG\" merchant_id=\"90028101\" amount=\"3100\"/></order></merchant><merchant_sign type=\"RSA\"/></customer><customer_sign type=\"SSL\">4817C411000100000084</customer_sign><results timestamp=\"2006-11-22 12:20:30\"><payment approval_code=\"730190\" card_bin=\"KAZ\" c_hash=\"6A2D7673A8EEF25A2C33D67CB5AAD091\" merchant_id=\"90050801\" reference=\"109600746891\" response_code=\"00\" Secure=\"No\" amount=\"320.5\"/></results></bank><bank_sign cert_id=\"c183d690\" type=\"SHA/RSA\">JI3RZMEvexNlDmKsOQhe0pzHuKijnbhvnLu99qh7h+Ju8HvSfGNbEJxXUL58M94tXvu7w0BXSY7MHePGqz32JuMLAncuzyMwq845linW/sH/WvbZ+6SSYfxDMnvgX0S/pKxbhSXs7lGVBngXOwq7Bhsk8GcDUkWAM5UAsKpEKoI=</bank_sign></document>";
+    private static final String TEST_DOCUMENT_AS_PLAINTEXT = ""
+	    + "<document>"
+	    + "<bank name=\"Kazkommertsbank JSC\">"
+	    + "<customer mail=\"klient@mymail.com\" name=\"John Cardholder\" phone=\"223322\">"
+	    + "<merchant cert_id=\"7269c18d00010000005e\" name=\"Shop Name\">"
+	    + "<order order_id=\"000282\" currency=\"398\" amount=\"3100\">"
+	    + "<department RL=\"ASDFG\" merchant_id=\"90028101\" amount=\"3100\"/>"
+	    + "</order>"
+	    + "</merchant>"
+	    + "<merchant_sign type=\"RSA\"/>"
+	    + "</customer>"
+	    + "<customer_sign type=\"SSL\">4817C411000100000084</customer_sign>"
+	    + "<results timestamp=\"2006-11-22 12:20:30\">"
+	    + "<payment approval_code=\"730190\" card_bin=\"KAZ\" c_hash=\"6A2D7673A8EEF25A2C33D67CB5AAD091\" merchant_id=\"90050801\" reference=\"109600746891\" response_code=\"00\" Secure=\"No\" amount=\"320.5\"/>"
+	    + "</results>"
+	    + "</bank>"
+	    + "<bank_sign cert_id=\"c183d690\" type=\"SHA/RSA\">JI3RZMEvexNlDmKsOQhe0pzHuKijnbhvnLu99qh7h+Ju8HvSfGNbEJxXUL58M94tXvu7w0BXSY7MHePGqz32JuMLAncuzyMwq845linW/sH/WvbZ+6SSYfxDMnvgX0S/pKxbhSXs7lGVBngXOwq7Bhsk8GcDUkWAM5UAsKpEKoI=</bank_sign>"
+	    + "</document>";
 
     static {
 	TEST_DOCUMENT_AS_OBJECT = new XmlDocumentPayment();
