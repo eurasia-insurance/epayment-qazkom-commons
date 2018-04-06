577e083f2594147428e6fa5b2446f7428fe32047
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-04-06 17:33:30 +0600

    builder

--- src/main/java/tech/lapsa/epayment/qazkom/xml/bind/XmlControlResonseDocument.java
+++ src/main/java/tech/lapsa/epayment/qazkom/xml/bind/XmlControlResonseDocument.java
@@ -1,6 +1,11 @@
 package tech.lapsa.epayment.qazkom.xml.bind;
 
 import java.math.BigInteger;
+import java.security.cert.X509Certificate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
 
 import javax.xml.bind.annotation.XmlAccessOrder;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -15,7 +20,12 @@ import tech.lapsa.epayment.qazkom.xml.bind.XmlControlDocument.XmlMerchant;
 import tech.lapsa.epayment.qazkom.xml.bind.XmlControlDocument.XmlMerchantSign;
 import tech.lapsa.epayment.qazkom.xml.bind.adapter.XmlCertificateSeriaNumberToHEXStringAdapter;
 import tech.lapsa.epayment.qazkom.xml.schema.XmlSchemas;
+import tech.lapsa.java.commons.function.MyArrays;
+import tech.lapsa.java.commons.function.MyExceptions;
+import tech.lapsa.java.commons.function.MyObjects;
 import tech.lapsa.java.commons.function.MyStrings;
+import tech.lapsa.java.commons.security.MySignatures;
+import tech.lapsa.java.commons.security.MySignatures.VerifyingSignature;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
@@ -25,7 +35,8 @@ public class XmlControlResonseDocument extends AXmlBase {
 
     private static final long serialVersionUID = 1L;
 
-    private static final SerializationTool<XmlControlResonseDocument> TOOL = SerializationTool.forClass(XmlControlResonseDocument.class, XmlSchemas.CONTROL_RESPONSE_SCHEMA);
+    private static final SerializationTool<XmlControlResonseDocument> TOOL = SerializationTool
+	    .forClass(XmlControlResonseDocument.class, XmlSchemas.CONTROL_RESPONSE_SCHEMA);
 
     public static final SerializationTool<XmlControlResonseDocument> getTool() {
 	return TOOL;
@@ -36,12 +47,90 @@ public class XmlControlResonseDocument extends AXmlBase {
 	return TOOL.deserializeFrom(rawXml);
     }
 
+    public static XmlControlResonseDocumentBuilder builder() {
+	return new XmlControlResonseDocumentBuilder();
+    }
+
+    public static final class XmlControlResonseDocumentBuilder {
+	private String rawXml;
+	private X509Certificate certificate;
+
+	private XmlControlResonseDocumentBuilder() {
+	}
+
+	public XmlControlResonseDocumentBuilder ofRawXml(final String rawXml) throws IllegalArgumentException {
+	    this.rawXml = MyStrings.requireNonEmpty(rawXml, "rawXml");
+	    return this;
+	}
+
+	public XmlControlResonseDocumentBuilder withBankCertificate(final X509Certificate certificate)
+		throws IllegalArgumentException {
+	    this.certificate = MyObjects.requireNonNull(certificate, "certificate");
+	    return this;
+	}
+
+	public XmlControlResonseDocument build() throws IllegalArgumentException, IllegalStateException {
+	    MyStrings.requireNonEmpty(rawXml, "rawXml");
+	    final XmlControlResonseDocument document = TOOL.deserializeFrom(rawXml);
+
+	    if (MyObjects.nonNull(certificate)) {
+
+		// checking signature
+		{
+		    final String[] banks = XmlBank.bankXmlElementsFrom(rawXml);
+		    if (banks.length != 1)
+			throw new IllegalArgumentException("Failed to parse for single element <bank> from source XML document");
+		    final byte[] data = banks[0].getBytes();
+		    final byte[] digest = document.getBankSign().getSignature();
+		    MyArrays.reverse(digest);
+		    final String algorithmName = document.getBankSign().getSignType().getSignatureAlgorithmName() //
+			    .orElseThrow(MyExceptions.illegalArgumentSupplier("No such alogithm for signature"));
+		    final VerifyingSignature signature = MySignatures.forVerification(certificate, algorithmName) //
+			    .orElseThrow(MyExceptions.illegalArgumentSupplier("Algorithm is not supported %1$s",
+				    algorithmName));
+		    final boolean signatureValid = signature.verify(data, digest);
+		    if (!signatureValid)
+			throw MyExceptions.illegalStateFormat("Signature is invalid");
+		}
+
+		// checking certificate number
+		{
+		    final boolean certNumberValid = certificate.getSerialNumber()
+			    .equals(document.getBankSign().getCertificateSerialNumber());
+		    if (!certNumberValid)
+			throw MyExceptions.illegalStateFormat("Certificate serial number is not valid");
+		}
+	    }
+	    return document;
+	}
+    }
+
     @XmlAccessorType(XmlAccessType.FIELD)
     @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
     @HashCodePrime(-1)
     public static class XmlBank extends AXmlBase {
 
 	private static final long serialVersionUID = 1L;
+
+	private static final String BANK_REGEX = "(\\<bank.*?\\<\\/bank\\>)";
+	private static final int BANK_REGEX_FLAGS = Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.MULTILINE
+		| Pattern.DOTALL;
+	private static final Pattern BANK_REGEX_PATTERN = Pattern.compile(BANK_REGEX, BANK_REGEX_FLAGS);
+
+	public static String[] bankXmlElementsFrom(final String rawXml) {
+	    final Matcher matcher = BANK_REGEX_PATTERN.matcher(rawXml);
+	    final Builder<String> sb = Stream.builder();
+	    while (matcher.find())
+		sb.accept(matcher.group());
+	    return sb.build().filter(xml -> {
+		try {
+		    return TOOL.deserializeFrom(xml) != null;
+		} catch (final IllegalArgumentException e) {
+		    return false;
+		}
+
+	    }).toArray(String[]::new);
+	}
 
 	@XmlAttribute(name = "name")
 	private final String name;
