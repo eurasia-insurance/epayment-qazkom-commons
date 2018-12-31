2e5b77601a1ae72d2fea3e866d000394009a71fd
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-12-31 12:37:57 +0300

    constants class

--- src/main/java/tech/lapsa/epayment/qazkom/xml/bind/adapter/XmlTimestampAdapter.java
+++ src/main/java/tech/lapsa/epayment/qazkom/xml/bind/adapter/XmlTimestampAdapter.java
@@ -2,28 +2,28 @@ package tech.lapsa.epayment.qazkom.xml.bind.adapter;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import tech.lapsa.epayment.qazkom.QazkomConstants;
 
 public class XmlTimestampAdapter extends XmlAdapter<String, Instant> {
 
     private static final String KKB_TIMESTAMP_PATTERN = "yyyy-MM-dd HH:mm:ss";
     private static final DateTimeFormatter KKB_TIMESTAMP_FORMATTER = DateTimeFormatter.ofPattern(KKB_TIMESTAMP_PATTERN);
-    private static final ZoneId KKB_ZONE = ZoneId.of("Asia/Almaty");
 
     @Override
     public Instant unmarshal(final String v) throws Exception {
 	if (v == null)
 	    return null;
-	return LocalDateTime.parse(v, KKB_TIMESTAMP_FORMATTER).atZone(KKB_ZONE).toInstant();
+	return LocalDateTime.parse(v, KKB_TIMESTAMP_FORMATTER).atZone(QazkomConstants.QAZKOM_ZONE_ID).toInstant();
     }
 
     @Override
     public String marshal(final Instant v) throws Exception {
 	if (v == null)
 	    return null;
-	return v.atZone(KKB_ZONE).format(KKB_TIMESTAMP_FORMATTER);
+	return v.atZone(QazkomConstants.QAZKOM_ZONE_ID).format(KKB_TIMESTAMP_FORMATTER);
     }
 }
